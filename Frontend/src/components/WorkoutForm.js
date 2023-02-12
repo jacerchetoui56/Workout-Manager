0a4cd9a805dcd2f45118e864cf6767d63ddeb7f3
jacer <jacerdjo@gmail.com>
2023-02-12 22:18:17 +0100

    adding a spinner and improved the code

--- Frontend/src/components/WorkoutForm.js
+++ Frontend/src/components/WorkoutForm.js
@@ -1,10 +1,11 @@
 import React, { useState } from "react";
-import { useWorkoutsContext } from "../hooks/useWorkoutsContext";
 import { ACTIONS } from "../context/WorkoutContext";
 import { useAuthContext } from "../hooks/useAuthContext";
+import { useWorkoutsContext } from "../hooks/useWorkoutsContext";
 
 export default function WorkoutForm() {
   const { user } = useAuthContext();
+  const [loading, setLoading] = useState(false);
   const [form, setForm] = useState({
     title: "",
     load: "",
@@ -26,32 +27,39 @@ export default function WorkoutForm() {
   }
 
   const handleSubmit = async (e) => {
+    setLoading(true);
     e.preventDefault();
 
     if (!user) {
       setError("You Must Be Logged In");
       return;
     }
-    const response = await fetch(
-      "https://workout-manager-jacer.onrender.com/api/workouts",
-      {
-        method: "POST",
-        body: JSON.stringify(form),
-        headers: {
-          authorization: `Bearer ${user?.token}`,
-          "Content-Type": "application/json",
-        },
+    try {
+      const response = await fetch(
+        "https://workout-manager-jacer.onrender.com/api/workouts",
+        {
+          method: "POST",
+          body: JSON.stringify(form),
+          headers: {
+            authorization: `Bearer ${user?.token}`,
+            "Content-Type": "application/json",
+          },
+        }
+      );
+
+      const json = await response.json();
+
+      if (!response.ok) {
+        setError(json.error);
+        console.log(error);
+      } else {
+        dispatch({ type: ACTIONS.ADD_WORKOUT, payload: json });
+        setForm({ title: "", load: "", reps: "" });
       }
-    );
-
-    const json = await response.json();
-
-    if (!response.ok) {
-      setError(json.error);
+    } catch (error) {
       console.log(error);
-    } else {
-      dispatch({ type: ACTIONS.ADD_WORKOUT, payload: json });
-      setForm({ title: "", load: "", reps: "" });
+    } finally {
+      setLoading(false);
     }
   };
   return (
@@ -59,6 +67,7 @@ export default function WorkoutForm() {
       <form onSubmit={handleSubmit}>
         <label htmlFor="title">Title</label>
         <input
+          required
           id="title"
           type="text"
           name="title"
@@ -68,6 +77,7 @@ export default function WorkoutForm() {
         />
         <label htmlFor="reps">Reps</label>
         <input
+          required
           id="reps"
           type="number"
           name="reps"
@@ -77,6 +87,7 @@ export default function WorkoutForm() {
         />
         <label htmlFor="load">Load (kg)</label>
         <input
+          required
           id="load"
           type="number"
           name="load"
@@ -84,7 +95,10 @@ export default function WorkoutForm() {
           placeholder="Load"
           value={form.load}
         />
-        <button type="submit">Add Workout</button>
+        <div className="button-loader">
+          <button type="submit">Add Workout</button>
+          {loading && <div className="spinner"></div>}
+        </div>
         {error && <div className="error">{error}</div>}
       </form>
     </div>
--- Frontend/src/pages/Login.js
+++ Frontend/src/pages/Login.js
@@ -1,59 +1,66 @@
-import React, { useState } from 'react'
-import useLogin from '../hooks/useLogin'
-
+import React, { useState } from "react";
+import useLogin from "../hooks/useLogin";
 
 export default function Login() {
+  const { login, error, isLoading } = useLogin();
+  const [loading, setLoading] = useState(false);
+  const [form, setForm] = useState({
+    email: "",
+    password: "",
+  });
 
-    const { login, error, isLoading } = useLogin()
-    const [form, setForm] = useState({
-        email: '',
-        password: ''
-    })
+  function handleChange(e) {
+    const { name, value } = e.target;
+    setForm((prev) => {
+      return {
+        ...prev,
+        [name]: value,
+      };
+    });
+  }
 
-    function handleChange(e) {
-        const { name, value } = e.target
-        setForm(prev => {
-            return {
-                ...prev,
-                [name]: value
-            }
-        })
+  async function handleSubmit(e) {
+    setLoading(true);
+    e.preventDefault();
+    try {
+      await login(form.email, form.password);
+      setForm({ email: "", password: "" });
+    } catch (error) {
+      console.log(error);
+    } finally {
+      setLoading(false);
     }
-
-    async function handleSubmit(e) {
-        e.preventDefault()
-
-        console.log(form)
-        await login(form.email, form.password)
-        setForm({ email: '', password: '' })
-    }
-    return (
-        <form onSubmit={handleSubmit} className='login'>
-            <h3>Login</h3>
-            <label htmlFor="email">Email:</label>
-            <input
-                type="email"
-                required
-                placeholder='Email'
-                onChange={handleChange}
-                value={form.email}
-                name='email'
-                id='email'
-            />
-            <label htmlFor="password">Password:</label>
-            <input
-                type="password"
-                placeholder='Password'
-                required
-                onChange={handleChange}
-                value={form.password}
-                name='password'
-                id='password'
-            />
-            <button type='submit' disabled={isLoading}>Sign Up</button>
-            {
-                error && <div className="error">{error}</div>
-            }
-        </form>
-    )
+  }
+  return (
+    <form onSubmit={handleSubmit} className="login">
+      <h3>Login</h3>
+      <label htmlFor="email">Email:</label>
+      <input
+        type="email"
+        required
+        placeholder="Email"
+        onChange={handleChange}
+        value={form.email}
+        name="email"
+        id="email"
+      />
+      <label htmlFor="password">Password:</label>
+      <input
+        type="password"
+        placeholder="Password"
+        required
+        onChange={handleChange}
+        value={form.password}
+        name="password"
+        id="password"
+      />
+      <div className="button-loader">
+        <button type="submit" disabled={isLoading}>
+          Login
+        </button>
+        {error && <div className="error">{error}</div>}
+        {loading && <div className="spinner"></div>}
+      </div>
+    </form>
+  );
 }
